623ae92438955864f762e998f632e647b0f51dd4
salahali2019 <54132280+salahali2019@users.noreply.github.com>
2023-03-02 17:50:51 +1100

    Create test_utils.py

---
+++ tests/test_utils.py
@@ -0,0 +1,27 @@
+import unittest
+import numpy as np
+from PIL import Image
+from models import inference, plot_confusion_matrix, plot_calibration_curve, save_false_positives, find_false_positive_patterns
+
+class TestMyModule(unittest.TestCase):
+    
+    def setUp(self):
+        # Initialize variables needed for tests
+    
+    def test_inference(self):
+        # Test inference function
+    
+    def test_plot_confusion_matrix(self):
+        # Test plot_confusion_matrix function
+    
+    def test_plot_calibration_curve(self):
+        # Test plot_calibration_curve function
+    
+    def test_save_false_positives(self):
+        # Test save_false_positives function
+
+    def test_find_false_positive_patterns(self):
+        # Test find_false_positive_patterns function
+
+if __name__ == '__main__':
+    unittest.main()
